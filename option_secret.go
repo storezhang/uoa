abaef4911935d9c8b7d0849f3eeb308ca930af98
storezhang <storezhang@gmail.com>
2021-05-24 20:03:55 +0800

    增加Secret配置

---
+++ option_secret.go
@@ -0,0 +1,28 @@
+package uoa
+
+var _ option = (*optionSecret)(nil)
+
+type optionSecret struct {
+	// 授权，类似于用户名
+	id string
+	// 授权，类似于密码
+	key string
+}
+
+// Secret 配置授权
+func Secret(id string, key string) *optionSecret {
+	return &optionSecret{
+		id:  id,
+		key: key,
+	}
+}
+
+// Tencentyun 配置腾讯云授权
+func Tencentyun(secretId string, secretKey string) *optionSecret {
+	return Secret(secretId, secretKey)
+}
+
+func (b *optionSecret) apply(options *options) {
+	options.secret.Id = b.id
+	options.secret.Key = b.key
+}
