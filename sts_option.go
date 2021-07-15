ac1f5df2b2400941e3d1d54a130b324e1aec2c9b
storezhang <storezhang@gmail.com>
2021-07-15 15:18:27 +0800

    增加临时密钥

--- sts_option.go
+++ sts_option.go
@@ -3,3 +3,8 @@ package uoa
 type stsOption interface {
 	applySts(options *stsOptions)
 }
+
+// NewStsOptions 创建选项，因为option接口不对外暴露，如果用户想在外面创建option并赋值将无法完成，特意提供创建option的快捷方式
+func NewStsOptions(opts ...stsOption) []stsOption {
+	return opts
+}
